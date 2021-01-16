7ee8c8bb159d3c04fafda9fdb844c990149df7e7
TheComputerGuy <rosscomputerguy@protonmail.com>
2021-01-16 12:06:19 -0800

    Replace exo with endo

--- panel-plugin/notification-plugin-dialogs.c
+++ panel-plugin/notification-plugin-dialogs.c
@@ -46,7 +46,7 @@ notification_plugin_configure_response (GtkWidget    *dialog,
 
   if (response == GTK_RESPONSE_HELP)
     {
-      result = g_spawn_command_line_async ("exo-open --launch WebBrowser " PLUGIN_WEBSITE, NULL);
+      result = g_spawn_command_line_async ("endo-open --launch WebBrowser " PLUGIN_WEBSITE, NULL);
 
       if (G_UNLIKELY (result == FALSE))
         g_warning (_("Unable to open the following url: %s"), PLUGIN_WEBSITE);
